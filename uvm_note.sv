e3d35a3c730e2d4ad212488f059fc042694d5857
enchantessss <lost.h@qq.com>
2018-10-06 13:06:29 +0800

    change

--- uvm_note.sv
+++ uvm_note.sv
@@ -1,4 +1,4 @@
-`uvm_info("MY_INFO1", $sformart("val: %0d",val), UVM_LOW);
+`uvm_info("MY_INFO1", $sformatf("val: %0d",val), UVM_LOW);
 `uvm_warning("MY_WARNING1", "This is a Warning");
 `uvm_error("MY_ERROR", "This is an error");
 `uvm_fatal("MY_FATAL", "A fatal error has occurred");
