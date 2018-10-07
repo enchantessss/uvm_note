1e678ff87394e3bd4160b4c19af71fba7267b1f2
enchantessss <lost.h@qq.com>
2018-10-07 19:25:42 +0800

    change

--- uvm_note.sv
+++ uvm_note.sv
@@ -73,3 +73,50 @@ in base_test or testcase 's build_phase();
 ...
 set_report_max_quit_count(num);
 ..
+
+uvm_config_db#(int)::set(this, "env.i_agt.drv", "key", 100);
+uvm_config_db#(int)::get(this, "", "key", var);
+
+-- get_full_name
+`uvm_info("FULL_NAME_DISPLAY", $sformatf("the full name of current component is %s", get_full_name()), UVM_LOW);
+
+// function phase
+build_phase
+connect_phase
+end_of_elaboration_phase
+start_of_simulation_phase
+    // task phase
+    pre_reset_phase
+    reset_phase
+    post_reset_phase
+    pre_configure_phase
+    configure_phase
+    post_configure_phase
+    pre_main_phase
+    main_phase
+    post_main_phase
+    pre_shutdown_phase
+    shutdown_phase
+    post_shutdown_phase
+// function phase
+extract_phase
+check_phase
+report_phase
+final_phase
+
+
+phase.jump(uvm_reset_phase::get());
+phase.jump(uvm_phase phase);
+phase can be:
+    uvm_build_phase::get();
+    uvm_connect_phase::get();
+    ...
+    uvm_run_phase::get();
+    ...
+    uvm_final_phase::get();
+
+
+in testcase's build_phase can set:
+    uvm_top.set_timeout(500ns,0);
+or use command:
+    ./simv +UVM_TIMEOUT="700ns,YES" // <time>,<overridable>
