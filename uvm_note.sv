6a3743cc4983568448f93dd72145eaf71c093fc4
enchantessss <lost.h@qq.com>
2018-10-08 01:07:53 +0800

    change

--- uvm_note.sv
+++ uvm_note.sv
@@ -120,3 +120,40 @@ in testcase's build_phase can set:
     uvm_top.set_timeout(500ns,0);
 or use command:
     ./simv +UVM_TIMEOUT="700ns,YES" // <time>,<overridable>
+
+in testcase's main_phase can set:
+    phase.phase_done.set_drain_time(this,200); //after #200 will enter post_main_phase
+
+in testcase's end_of_elaboration_phase can set:
+    uvm_top.print_topology();
+
+class A extends uvm_component;
+    uvm_domain new_domain();
+    `uvm_component_utilis(A)
+
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+        new_domain = new("new_domain");
+    endfunction
+
+    virtual function void connect_phase(uvm_phase phase);
+        set_domain(new_domain);
+    endfunction
+endclass
+
+>tar -xvzf xx.tar.gz
+
+
+`uvm_do(SEQ_OR_ITEM); is equal to:
+
+class case0_sequence extends uvm_sequence #(my_transaction);
+...
+    virtual task body();
+    ...
+        tr = new();
+        start_item(tr,priority_var); // priority_var reserve is -1
+        assert(tr.randomize() with {tr.pld_size == 200;});
+        finish_item(tr,priority_var);
+    ...
+    endtask
+endclass
